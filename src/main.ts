a03e2ac73bcc5b5d0e99175155250477042e2c5c
Zizo Majiza <majizazizo955@gmail.com>
2026-04-16 05:27:20 +0200

    fix build for render

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:3001',
-    methods: 'GET,POST,PATCH,DELETE',
-    credentials: true,
+    origin: ['http://localhost:3001', 'http://localhost:3000'],
   });
-
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 
   console.log('Server running on http://localhost:3000');
 }
