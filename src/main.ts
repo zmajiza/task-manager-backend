f642f115b696972ea78a8626b98267a7a29f140e
Zizo Majiza <majizazizo955@gmail.com>
2026-04-17 11:14:55 +0200

    fix: add Procfile for AWS

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,17 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:3001', 'http://localhost:3000'],
+    origin: [
+      'http://localhost:3000',
+      'https://task-manager-frontend-lq7f.onrender.com',
+    ],
+    methods: ['GET', 'POST', 'PATCH', 'DELETE'],
+    credentials: true,
   });
+
   await app.listen(process.env.PORT || 3000);
 
-  console.log('Server running on http://localhost:3000');
+  console.log('Server running');
 }
 
-void bootstrap();
+bootstrap();
